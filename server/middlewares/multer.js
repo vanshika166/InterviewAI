e24f2c8ee9b2bc505a306bed395973deccb8835d
Vanshika <vanshikakashyap769@gmail.com>
2026-04-22 22:04:40 +0530

    fixed multer file  path

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,9 +1,16 @@
 import path from 'path';
 import multer from 'multer';
+import fs from "fs";
+
+const uploadPath = path.join(process.cwd(), "public/uploads");
+
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(process.cwd(), "/public/uploads"));
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
         const fileName = Date.now() + "-" + file.originalname;
